2c1b7d659a2305326c3f0bc8c8a4b6e318178323
Wastayo <omotayo567@gmail.com>
2023-07-15 08:12:16 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -this is the main function
+ *
+ * Return: always o
+ */
+
+int main(void)
+{
+	puts("\"programming is the like building a multilingual puzzle");
+	return (0);
+}
+
