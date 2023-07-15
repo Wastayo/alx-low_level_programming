bbb90728ca46282f6a9718c266e946b70450c671
Wastayo <omotayo567@gmail.com>
2023-07-15 16:29:12 -0700

    100-intel

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - A programme that prints the size of variuos data types
+ * Return: 0 (sucess)
+ * /
+
+int main(void)
+{
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
+	printf("Size of an int: %i byte(s)\n", sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+	printf("Size of float: %i byte(s)\n", sizeof(float));
+	return (0);
+}
