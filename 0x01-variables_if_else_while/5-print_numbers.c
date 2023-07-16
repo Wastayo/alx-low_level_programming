bcc907cb480641f93c531265a69f35f17add900d
Wastayo <omotayo567@gmail.com>
2023-07-16 15:16:40 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * Description: The main function prints out number less than 10
+  * Return: This returns 0
+  */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%i", n);
+	}
+	printf("\n");
+	return (0);
+}
