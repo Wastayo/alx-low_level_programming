e97f90576224e8b5d2c0e27144e48437a5a0424c
Wastayo <omotayo567@gmail.com>
2023-07-19 13:40:49 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * print_alphabet - prototype function
+  * Description: This prints out aphabet from a-z
+  * in lower case
+  * Return: this returns void
+  */
+
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
