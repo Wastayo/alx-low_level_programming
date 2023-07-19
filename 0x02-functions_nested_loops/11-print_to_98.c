e110367682f64d03be86d51bbc2202817d7c9160
Wastayo <omotayo567@gmail.com>
2023-07-19 14:11:19 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  * print_to_98 - prints out a certain numbe
+  * @num: a parameter to determine increament or decrement
+  * Dexcription: this print out a value by incrementing it  from @num to 98 if
+  * @num is lower than 98
+  * if greater it prints a value by  decreaseing it from @num down to 98
+  * Return: returns void;
+  */
+
+
+void print_to_98(int num)
+{
+	int number = num;
+
+	if (num < 98)
+	{
+		while (number <= 98)
+		{
+			if (number == 98)
+			{
+				printf("%i", number);
+			}
+			else
+			{
+				printf("%i, ", number);
+			}
+			number++;
+		}
+	}
+	else
+	{
+		while (number >= 98)
+		{
+			if (number == 98)
+			{
+				printf("%i", number);
+			}
+			else
+			{
+				printf("%i, ", number);
+			}
+			number--;
+		}
+	}
+	printf("\n");
+
+}
